f8060340d225513dee2fda0948ebf4b40ccb4f97
nhan nguyen <nhanngyn2004@gmail.com>
2025-07-28 16:38:38 -0700

    updated website

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -11,7 +11,7 @@ export const Home = () => {
                 {/* Profile Picture */}
                 <div className="mb-10 flex justify-center">
                     <img 
-                        src="/images/profile.jpg" 
+                        src="/images/profile.png" 
                         alt="Nhan Nguyen" 
                         className="w-50 h-50 md:w-70 md:h-70 rounded-full border-4 border-blue-500 shadow-lg hover:shadow-[0_0_20px_rgba(59,130,246,0.4)] transition-all duration-300 hover:scale-105"
                     />
